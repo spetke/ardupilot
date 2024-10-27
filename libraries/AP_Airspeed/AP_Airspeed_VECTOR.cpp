f476f5000a00fcd009b14fa8f2979444b3ae3dbf
Spetke <ph@c0y.org>
2024-10-27 17:20:37 +0200

    new base

--- libraries/AP_Airspeed/AP_Airspeed_VECTOR.cpp
+++ libraries/AP_Airspeed/AP_Airspeed_VECTOR.cpp
@@ -12,44 +12,36 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
-Driver for VectorSave Airspeed sensor   
- */
+
 
 #include "AP_Airspeed_VECTOR.h"
 
 #if AP_AIRSPEED_VECTOR_ENABLED
 
-#include "AP_Airspeed.h"
-#include <AP_HAL/AP_HAL.h>
-#include <AP_HAL/utility/OwnPtr.h>
 #include <AP_HAL/I2CDevice.h>
-#include <utility>
 
-#define TIMEOUT_MS 2000
 extern const AP_HAL::HAL &hal;
 
-#define VECTOR_I2C_ADDR_8B 0x50
-#define VECTOR_I2C_ADDR_7B 0x28
-#define REG_CMD 0x30
-#define REG_PRESS_DATA 0x06
-#define REG_TEMP_DATA 0x09
-#define REG_PART_ID 0x01
-#define REG_PART_ID_SET 0xa4
-#define REG_SENSOR_READY 0x08
-#define REG_WHOAMI_DEFAULT_ID 0X00
-#define REG_WHOAMI_RECHECK_ID 0X66
-#define CMD_MEASURE 0x0A
+#define VECTOR_I2C_ADDR_1      0x28
+#define VECTOR_I2C_ADDR_2      0x40
+#define REG_CMD                0x30
+#define REG_PRESS_DATA         0x06
+#define REG_TEMP_DATA          0x09
+#define REG_PART_ID            0x01
+#define REG_PART_ID_SET        0xa4
+#define REG_SENSOR_READY       0x08
+#define REG_WHOAMI_DEFAULT_ID  0X00
+#define REG_WHOAMI_RECHECK_ID  0X66
+#define CMD_MEASURE            0x0A
+
 
 bool AP_Airspeed_VECTOR::init()
 {
     // probe the sensor, supporting multiple possible I2C addresses
-    const uint8_t addresses[] = {VECTOR_I2C_ADDR_7B, VECTOR_I2C_ADDR_8B};
-    for (uint8_t address : addresses)
-    {
+    const uint8_t addresses[] = { VECTOR_I2C_ADDR_1, VECTOR_I2C_ADDR_2 };
+    for (uint8_t address : addresses) {
         dev = hal.i2c_mgr->get_device(get_bus(), address);
-        if (!dev)
-        {
+        if (!dev) {
             continue;
         }
 
@@ -57,15 +49,14 @@ bool AP_Airspeed_VECTOR::init()
         dev->set_speed(AP_HAL::Device::SPEED_HIGH);
         dev->set_retries(2);
 
-        if (!confirm_sensor_id())
-        {
+        if (!confirm_sensor_id()) {
             continue;
         }
 
         dev->set_device_type(uint8_t(DevType::VECTOR));
         set_bus_id(dev->get_bus_id());
 
-        dev->register_periodic_callback(1000000UL / 80U,
+        dev->register_periodic_callback(10000,
                                         FUNCTOR_BIND_MEMBER(&AP_Airspeed_VECTOR::timer, void));
         return true;
     }
@@ -73,12 +64,114 @@ bool AP_Airspeed_VECTOR::init()
     // not found
     return false;
 }
-// read the from the sensor
-bool AP_Airspeed_VECTOR::get_airspeed(float &airspeed)
+
+/*
+  this sensor has an unusual whoami scheme. The part_id is changeable
+  via another register. We check the sensor by looking for the
+  expected behaviour
+*/
+bool AP_Airspeed_VECTOR::confirm_sensor_id(void)
 {
-
-
-    return (now - _last_update_ms) < TIMEOUT_MS;
+    uint8_t part_id;
+    if (!dev->read_registers(REG_PART_ID_SET, &part_id, 1) ||
+        ( (part_id != REG_WHOAMI_DEFAULT_ID) && (part_id != REG_WHOAMI_RECHECK_ID) ) ) {
+        return false;
+    }
+    if (!dev->write_register(REG_PART_ID_SET, REG_WHOAMI_RECHECK_ID)) {
+        return false;
+    }
+    if (!dev->read_registers(REG_PART_ID, &part_id, 1) ||
+        part_id != REG_WHOAMI_RECHECK_ID) {
+        return false;
+    }
+    return true;
 }
 
-#endif  // AP_AIRSPEED_VECTOR_ENABLED
+
+// read the data from the sensor
+void AP_Airspeed_VECTOR::timer()
+{
+    // request a new measurement cycle begin
+    dev->write_register(REG_CMD, CMD_MEASURE);
+
+    uint8_t status;
+    if (!dev->read_registers(REG_CMD, &status, 1) ||
+        (status & REG_SENSOR_READY) == 0) {
+        // no data ready
+        return;
+    }
+
+    // read pressure and temperature as one block
+    uint8_t data[5];
+    if (!dev->read_registers(REG_PRESS_DATA, data, sizeof(data))) {
+        return;
+    }
+
+    // ADC pressure is signed 24 bit
+    int32_t press = (data[0]<<24) | (data[1]<<16) | (data[2]<<8);
+
+    // convert back to 24 bit
+    press >>= 8;
+
+    // k is a shift based on the pressure range of the device. See
+    // table in the datasheet
+    constexpr uint8_t k = 7;
+    constexpr float press_scale = 1.0 / (1U<<k);
+
+    // temperature is 16 bit signed in units of 1/256 C
+    const int16_t temp = (data[3]<<8) | data[4];
+    constexpr float temp_scale = 1.0 / 256;
+
+    WITH_SEMAPHORE(sem);
+    press_sum += press * press_scale;
+    temp_sum += temp * temp_scale;
+    press_count++;
+    temp_count++;
+
+    last_sample_ms = AP_HAL::millis();
+}
+
+
+// return the current differential_pressure in Pascal
+bool AP_Airspeed_VECTOR::get_differential_pressure(float &pressure)
+{
+    WITH_SEMAPHORE(sem);
+
+    if (AP_HAL::millis() - last_sample_ms > 100) {
+        return false;
+    }
+
+    if (press_count == 0) {
+        pressure = last_pressure;
+        return true;
+    }
+
+    last_pressure = pressure = press_sum / press_count;
+
+    press_count = 0;
+    press_sum = 0;
+
+    return true;
+}
+
+// return the current temperature in degrees C, if available
+bool AP_Airspeed_VECTOR::get_temperature(float &temperature)
+{
+    WITH_SEMAPHORE(sem);
+
+    if (AP_HAL::millis() - last_sample_ms > 100) {
+        return false;
+    }
+    if (temp_count == 0) {
+        temperature = last_temperature;
+        return true;
+    }
+
+    last_temperature = temperature = temp_sum / temp_count;
+    temp_count = 0;
+    temp_sum = 0;
+
+    return true;
+}
+
+#endif
--- libraries/AP_Airspeed/AP_Airspeed_VECTOR.h
+++ libraries/AP_Airspeed/AP_Airspeed_VECTOR.h
@@ -1,28 +1,45 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
 
 #include "AP_Airspeed_config.h"
 
 #if AP_AIRSPEED_VECTOR_ENABLED
-#include <AP_HAL/AP_HAL.h>
-#include <AP_HAL/utility/OwnPtr.h>
-#include <AP_HAL/I2CDevice.h>
-#include <utility>
+
+/*
+  backend driver for airspeed from I2C
+ */
 #include "AP_Airspeed_Backend.h"
 
 class AP_Airspeed_VECTOR : public AP_Airspeed_Backend
 {
 public:
-
     using AP_Airspeed_Backend::AP_Airspeed_Backend;
-
-    // probe and initialise the sensor
-    bool init(void) override;
-
-    // this reads airspeed directly
+    bool init() override;
+	    // this reads airspeed directly
     bool has_airspeed() override {return true;}
 
     // read the from the sensor
     bool get_airspeed(float &airspeed) override;
+
+
+    // return the current differential_pressure in Pascal
+    bool get_differential_pressure(float &_pressure) override;
+
+    // return the current temperature in degrees C, if available
+    bool get_temperature(float &_temperature) override;
 
 private:
     void timer();
@@ -36,12 +53,6 @@ private:
     uint32_t last_sample_ms;
 
     AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev;
-    // store last sent speed and temp as update rate is slow
-    float _last_temp;
-    float _last_speed;
-
-    // time last message was received
-    uint32_t _last_update_ms;
 };
 
 #endif  // AP_AIRSPEED_VECTOR_ENABLED
