858e1cd4dbe360dedb62f549bf8e20f300307b5f
Spetke <ph@c0y.org>
2024-11-05 13:30:29 +0200

    code compiles now with all necessary parts

--- libraries/AP_Airspeed/AP_Airspeed_VECTOR.cpp
+++ libraries/AP_Airspeed/AP_Airspeed_VECTOR.cpp
@@ -26,8 +26,9 @@
 
 extern const AP_HAL::HAL &hal;
 
-#define ASP5033_I2C_ADDR_1 0x6C
-#define ASP5033_I2C_ADDR_2 0x6D
+#define VECTOR_I2C_ADDR_1 0x28
+#define VECTOR_I2C_ADDR_2 0x40
+#define REG_SPEED_DATA 0x28
 #define REG_CMD 0x30
 #define REG_PRESS_DATA 0x06
 #define REG_TEMP_DATA 0x09
@@ -41,7 +42,7 @@ extern const AP_HAL::HAL &hal;
 bool AP_Airspeed_VECTOR::init()
 {
     // probe the sensor, supporting multiple possible I2C addresses
-    const uint8_t addresses[] = {ASP5033_I2C_ADDR_1, ASP5033_I2C_ADDR_2};
+    const uint8_t addresses[] = {VECTOR_I2C_ADDR_1, VECTOR_I2C_ADDR_2};
     for (uint8_t address : addresses)
     {
         dev = hal.i2c_mgr->get_device(get_bus(), address);
@@ -53,10 +54,11 @@ bool AP_Airspeed_VECTOR::init()
         WITH_SEMAPHORE(dev->get_semaphore());
         dev->set_speed(AP_HAL::Device::SPEED_HIGH);
         dev->set_retries(2);
+
         dev->set_device_type(uint8_t(DevType::VECTOR));
         set_bus_id(dev->get_bus_id());
 
-        dev->register_periodic_callback(1000000UL / 80U,
+        dev->register_periodic_callback(10000,
                                         FUNCTOR_BIND_MEMBER(&AP_Airspeed_VECTOR::timer, void));
         return true;
     }
@@ -67,9 +69,6 @@ bool AP_Airspeed_VECTOR::init()
 // read the data from the sensor
 void AP_Airspeed_VECTOR::timer()
 {
-    // request a new measurement cycle begin
-    dev->write_register(REG_CMD, CMD_MEASURE);
-
     uint8_t status;
     if (!dev->read_registers(REG_CMD, &status, 1) ||
         (status & REG_SENSOR_READY) == 0)
@@ -79,26 +78,20 @@ void AP_Airspeed_VECTOR::timer()
     }
 
     // read pressure and temperature as one block
-    uint8_t data[5];
-    if (!dev->read_registers(REG_PRESS_DATA, data, sizeof(data)))
+    uint8_t data[2];
+    if (!dev->read_registers(REG_SPEED_DATA, data, sizeof(data)))
     {
         return;
     }
 
-    // ADC pressure is signed 24 bit
-    //int32_t press = (data[0] << 24) | (data[1] << 16) | (data[2] << 8);
-
-    // convert back to 24 bit
-    //press >>= 8;
-
-    // temperature is 16 bit signed in units of 1/256 C
-    const int16_t temp = (data[3] << 8) | data[4];
-    constexpr float temp_scale = 1.0 / 256;
-
+    // speed  is signed 16 bit
+    int16_t speed = (data[0] << 8) | data[1];
+    const int16_t temp = (data[0] << 8) | data[1];
     WITH_SEMAPHORE(sem);
-    temp_sum += temp * temp_scale;
+    speed_sum += speed;
+    speed_count++;
+    temp_sum += temp;
     temp_count++;
-
     last_sample_ms = AP_HAL::millis();
 }
 
